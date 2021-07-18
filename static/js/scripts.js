938ab812eec6b69a341cd9587f84a5e2c609b934
arpit1ug <arpit1ug@gmail.com>
2021-07-18 20:55:02 +0530

    Add files via upload

---
+++ static/js/scripts.js
@@ -0,0 +1,58 @@
+// $( "#submit_button" ).on("click",function( event ) {
+//     var $form = $(this);
+//     var $error = $form.find(".error");
+//     var data = $form.serialize();
+
+//     $.ajax({
+//         url:"/user/signup/",
+//         type: "POST",
+//         data: data,
+//         dataType: "json",
+//         success: function(resp){
+//             console.log(resp);
+//         },
+//         error: function(resp){
+//             console.log(resp);
+//         }
+//     });
+// });
+$("form[name=signup_form").submit(function(e){
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+
+    $.ajax({
+        url:"/user/signup/",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(resp){
+            window.location.href="/dashboard/";
+        },
+        error: function(resp){
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
+
+    e.preventDefault();
+});
+$("form[name=login_form").submit(function(e){
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+
+    $.ajax({
+        url:"/user/login/",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(resp){
+            window.location.href="/dashboard/";
+        },
+        error: function(resp){
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
+
+    e.preventDefault();
+});
